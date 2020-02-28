60f364e95455dc3fb406ff84ed70c399ecd36a20
Rnugg <jordans11409@yahoo.com>
2020-02-28 12:49:05 -0800

    finished projet
    handling form data and using ViewBag with an ASP.NET Core

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("result")]
+        public IActionResult Result(string Name, string Location, string FavLanguage, string Comment)
+        {
+            ViewBag.Name = Name;
+            ViewBag.Location = Location;
+            ViewBag.FavLang = FavLanguage;
+            ViewBag.Comment = Comment;
+            return View();
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DojoSurvey.Models
+{
+    public class Survey
+    {
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(5)]
+        public string Location { get; set; }
+        
+        public string FavLanguage { get; set; }
+        public string Comment { get; set; } 
+    }
+}
